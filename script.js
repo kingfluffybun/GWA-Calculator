569b71a51cbd8c452a2fb26cce4ffac8f2d5f5b5
Fluffybun <Clarence.luna651@gmail.com>
2026-06-06 15:57:03 +0800

    added approximate gwa

--- script.js
+++ script.js
@@ -68,8 +68,13 @@ calculateGWA.addEventListener("click", function(){
         <div style="width: 100%; height: 2px; background-color: #e0e0e0; margin: 2px 0 14px 0;"></div>
         <p class="header" style="margin: 16px 0; font-weight: 700;">Result</p>
         <div class="gwa-content">
-            <p>General Weighted Average</p>
+            <p style="color:#f5f5f5">General Weighted Average</p>
             <h1 id="gwa" style="font-size: 48px;">${gwa.toFixed(2)}</h1>
+            <div style="display: flex; align-items:center; gap:4px; color:#f0f0f0">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-equal-approximately-icon lucide-equal-approximately"><path d="M5 15a6.5 6.5 0 0 1 7 0 6.5 6.5 0 0 0 7 0"/><path d="M5 9a6.5 6.5 0 0 1 7 0 6.5 6.5 0 0 0 7 0"/></svg>
+                <p style="font-size:14px">${gwa.toFixed(4)}</p>
+            </div>
+            
         </div>
     `
     container.appendChild(result)
